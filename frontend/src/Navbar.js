38ce0c495efb0b073fcdf3f69d578b83a67731d3
Adriana <am20dipi@alum.siena.edu>
2021-07-31 15:13:52 -0400

    Creating navbar.js file, renderin navbar in app.js, install react-router-dom for Navlink and router use, adding paths to App.js to eventual routes

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'pink',
+    textDecoration: 'none',
+    color: 'black',
+  }
+  
+export default class Navbar extends React.Component {
+    render() {
+        return (
+            <div class="navbar">
+            <NavLink
+                to="/"
+                exact
+                style={link}
+                activeStyle={{
+                background: 'red'
+                }}
+            >Home</NavLink>
+            <NavLink
+                to="/login"
+                exact
+                style={link}
+                activeStyle={{
+                background: 'red'
+                }}
+            >Login</NavLink>
+            <NavLink
+                to="/signup"
+                exact
+                style={link}
+                activeStyle={{
+                background: 'red'
+                }}
+            >Signup</NavLink>
+            <NavLink
+                to="/squares"
+                exact
+                style={link}
+                activeStyle={{
+                background: 'red'
+                }}
+            >Squares</NavLink>
+            </div>
+        )
+        
+    }
+}
