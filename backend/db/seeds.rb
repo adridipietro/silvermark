3c2ff4c485f1851203db2efb85306a669b1cfa04
Adriana <am20dipi@alum.siena.edu>
2021-07-31 14:16:27 -0400

    Creating seed data, testing out data in rails console

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+bookmarks = Bookmark.create([
+    {
+        headline: "New to Tech? Try out these free courses!",
+        web_url: "www.freecourses.com", 
+        description: "Free courses offered to people entering the technology industry."
+    },
+    {
+        headline: "Build Your Portfolio Like a Pro!",
+        web_url: "www.portfoliohelp.com", 
+        description: "Tips and secrets to building a really good portfolio."
+    },
+    {
+        headline: "New Vegan Recipes For Those Who Cannot Cook",
+        web_url: "www.veganrecipes.com", 
+        description: "Vegan recipe blog"
+    },
+    {
+        headline: "Pets for Adoption Near You",
+        web_url: "www.petsmart.com", 
+        description: "Website that shows pets in your area up for adoption."
+    },
+])
