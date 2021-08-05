5e369bc1c85a9741925bc3af38d3faf3b8e4628f
Adriana <am20dipi@alum.siena.edu>
2021-08-05 17:41:32 -0400

    Generating and migrating resource Category

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :users
   get 'home/index'
   resources :bookmarks
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_180444) do
+ActiveRecord::Schema.define(version: 2021_08_05_214106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,13 +23,12 @@ ActiveRecord::Schema.define(version: 2021_07_31_180444) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "squares", force: :cascade do |t|
-    t.string "color"
-    t.integer "height"
-    t.integer "width"
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_07_31_180444) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "categories", "users"
 end
