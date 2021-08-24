f37905a182a6145a04665af95596391073726ac4
Adriana <am20dipi@alum.siena.edu>
2021-08-24 13:07:20 -0400

    Updating cors file

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*',
-      headers: %w(Authorization),
-      methods: :any,
-      expose: %w(Authorization),
-      max_age: 600
+    resource(
+     '*',
+     headers: :any,
+     expose: ["Authorization"],
+     methods: [:get, :patch, :put, :delete, :post, :options, :show]
+    )
   end
 end
